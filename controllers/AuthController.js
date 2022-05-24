db995675aea92fca2050c950fe74bf03e73672f7
Ahmet Esad Barut <dwaebtdt@gmail.com>
2022-05-24 15:38:55 +0300

    Add login and verify mail

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,8 +1,7 @@
-import crypto from "crypto";
 import cron from "node-cron";
 import asyncHandler from "../middlewares/async";
 import verifyMail from "../utilis/verifyMail";
-import sendMail from "../utilis/SendMail";
+import client from "../config/redis";
 import User from "../models/UserModel";
 
 export const RegisterUser = asyncHandler(async (req, res, next) => {
@@ -25,7 +24,7 @@ export const RegisterUser = asyncHandler(async (req, res, next) => {
 
     await verifyMail(options);
     var job = cron.schedule(
-      "59 * * * *",
+      "* 2 * * *",
       async () => {
         try {
           const user1 = await User.findOne({ email: newUser.email });
@@ -43,13 +42,50 @@ export const RegisterUser = asyncHandler(async (req, res, next) => {
       { scheduled: false }
     );
     job.start();
-    res
-      .status(200)
-      .send({
-        status: "success",
-        message: "Doğrulama Kodu e-postanıza gönderildi.",
-      });
+    res.status(200).send({
+      status: "success",
+      message: "Doğrulama Kodu e-postanıza gönderildi.",
+    });
   } catch (error) {
     throw res.status(500).send("Doğrulama e-postası gönderilemedi");
   }
 });
+
+export const verifyEmail = asyncHandler(async (req, res, next) => {
+  const user = await User.findOne({ uid: req.body.code });
+  if (!user) throw res.status(401).send("Kod geçersiz");
+
+  if (user.verify) throw res.status(401).send("Zaten doğrulandı");
+  user.verify = true;
+
+  await user.save({ validateBeforeSave: false });
+  sendTokenResponse(user, 200, res);
+});
+
+export const login = asyncHandler(async (req, res, next) => {
+  const user = await User.findOne({
+    email: req.body.email,
+    verify: true,
+  }).select("+password");
+  if (!user) throw res.status(401).send("Email bulunamadı");
+
+  const isPassword = await user.matchPassword(req.body.password);
+  if (!isPassword) throw res.status(401).send("Parola yanlış");
+
+  sendTokenResponse(user, 200, res);
+});
+
+const sendTokenResponse = async (user, statusCode, res) => {
+  await user.genAuthToken();
+  const userData = {
+    id: user._id,
+    name: user.name,
+    email: user.email,
+    role: user.role,
+    verify: user.verify,
+  };
+  const data = await client.get(user._id.toHexString());
+  const token = JSON.parse(data);
+
+  res.status(statusCode).send({ status: "success", token, authData: userData });
+};
